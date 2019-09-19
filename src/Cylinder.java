eea05fdd1d26bfe1b8800bb1a3ab8d11604cd44f
Admin <admin@Admins-MacBook-Pro.local>
2019-09-19 16:40:10 +0700

    add

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,8 +1,17 @@
 public class Cylinder {
     public static double getVolume(int radius, int height){
-        double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
+        double baseArea = Cylinder.getBaseArea(radius);
+        double perimeter = Cylinder.getPerimeter(radius);
         double volume = perimeter * height + 2 * baseArea;
         return volume;
     }
+
+    private static double getBaseArea(int radius){
+        return Math.PI * radius * radius;
+    }
+
+    private static double getPerimeter(int radius){
+        return 2 * Math.PI  * radius;
+    }
+
 }
